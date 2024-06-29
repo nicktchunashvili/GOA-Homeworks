8494f006d23076b24b3f7082f4be2338cdb9a3ac
nick tchunashvili <nicktchunashvili@gmail.com>
2024-06-30 03:44:19 +0400

    eleventh commit

---
+++ Day10-Homework/lesson10.py
@@ -0,0 +1,89 @@
+#დათა თეზელაშვილის მიერ მოცემული დავალებები 
+
+#davaleba 1
+for i in range(20 + 1):
+    print(i)
+
+#davaleba 2
+sum = 0
+
+for num in range(50,100 + 1):
+    print(num)
+    sum = sum +num
+    print(sum)
+
+#davaleba 3 step-ები
+for i in range(-10,10 + 1 ,3):
+    print(i)
+
+
+#ბატონი ლუკა ცხვარაძის მიერ მოცემული დავალება:მოვიფიქროტ 10 მაგალიტი for ციკლისთვის
+
+#davaleba 1
+
+for i in range(1,6):
+    print(i)
+
+#davaleba 2
+
+sum = 0
+for num in range(10,21):
+    print(num)
+
+#davaleba 3
+
+sum = 0 
+for num in range(10,21):
+    print(num)
+    sum = num + sum
+    print(sum)
+
+#davaleba 4 step-ები
+
+for i in  range(10 , 31, 5):
+    print(i)
+
+#davaleba 5
+
+for num in range(2,5):
+    print(num)
+    print(range(2,5))
+
+
+#davaleba 6
+
+for num in range(2,5):
+    print(num)
+print(range(2,5))     #outside of the for loop
+
+
+#davaleba 7
+
+print(range(3,5))  #outside of the for loop
+
+for num in range(3,5):
+    print(num)
+
+
+#davaleba 8 
+
+sum = 0
+
+for num in range(2 , 20 + 1):
+    sum = sum + num
+
+
+#davaleba 9
+
+sum = 0 
+for num in range(10):
+    print(num)
+    print("girogi")
+    sum = sum + num
+
+#davaleba 10
+
+for i in range(100):
+    print("Nika")
+
+
