3739f5c37c643ba393ac2936abf26c73d0192161
nick tchunashvili <nicktchunashvili@gmail.com>
2024-06-30 03:43:40 +0400

    tenth commit

---
+++ Day09-Homework/Day09-Classwork/classwork9.py
@@ -0,0 +1,35 @@
+#task 1
+i = 1
+
+while i <= 10:
+    print(i)
+    i = i + 1
+
+#task 2
+i = 100
+
+while i >= 0:
+    print(i)
+    i = i - 1
+
+#task 3
+i = 1
+
+while i <= 10:
+    if i % 2 == 0:
+          print(i, "is even")
+    else:
+         print(i, "is odd")
+    print(i)
+    i = i + 1
+
+#task 4
+i = 1
+
+while i <= 10:
+     print(i)
+     i = i + 2
+
+
+
+       
