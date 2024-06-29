0b2791f823d14404271033ce61ff3f0fc02905bd
nick tchunashvili <nicktchunashvili@gmail.com>
2024-06-30 03:33:51 +0400

    fourth commit

---
+++ Day03-Homework/lesson3.py
@@ -0,0 +1,24 @@
+
+#mom
+
+mom_name = "keti"
+mom_surname = "Xaxutashvili"
+
+#dad
+
+dad_name = "Giorgi"
+dad_surname = "Tchunashvili"
+
+#Me
+
+My_name = "Nick"
+My_surname = "Tchunashvili"
+
+#broher
+
+brother_name = "Luka"
+brother_surname = "Tchunashvili"
+
+
+print(mom_name + " " + mom_surname + " " + dad_name + " " + dad_surname + " " + 
+      My_name + " " + My_surname + " " + brother_name + " " + brother_surname )
