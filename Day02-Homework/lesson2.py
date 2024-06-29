54ea8f01d8193c7f8729bfeb4f6e647967f5c517
nick tchunashvili <nicktchunashvili@gmail.com>
2024-06-30 03:32:31 +0400

    third commit

---
+++ Day02-Homework/lesson2.py
@@ -0,0 +1,6 @@
+print("გავიცანი და დავუმეგობრდი:Davit Janashia,  Tazo Gogishvili, Luka Gvinjilia, Vako Gelbakhiani, Cotne Nebieridze, Giorgi Bujiashvili, Nika Dodashvili")
+
+print("რას ვფიქრობ გოაზე<3:me goaze vfikrob rom es aris ara marto saswavlebeli da ubralo gakvetilebi aramed ojaxi sadac shecdomebze aravin dagsjis, aravin gechxubeba imaze tu ragac ver gaige, gaicnob bevr axal megobars da rac mtavaria bevr kompleqss moixsni, aseve kvelaze mnishvnelovania megobruli garemo da aravin gagakritikebs sheni azris gamotkmis dros amitom vfikrob rom goa sauketesoa, sauketeso mentorit<3")
+
+print("yes mshoblebs chavexute :D <3")
+
