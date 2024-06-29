38d132f7c461ddcb7efefa0288042fd5be6adfe0
nick tchunashvili <nicktchunashvili@gmail.com>
2024-06-30 03:31:56 +0400

    second commit

---
+++ Day01-Homework/lesson1.py
@@ -0,0 +1,15 @@
+name = "nika"
+surname = "tchunashvili"
+age = 15
+height = 180
+city = "Kvareli"
+
+
+knows_programming = True
+is_ugly = False
+
+
+
+print(name + " " + surname)
+
+print(name + " " + surname + " " + str(age) + " " + city)
