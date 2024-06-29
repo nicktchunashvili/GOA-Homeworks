2f481676ac40785b5573404f61638e4b2c9646b1
nick tchunashvili <nicktchunashvili@gmail.com>
2024-06-30 03:41:37 +0400

    eighth commit

---
+++ Day07-Homework/Day07-Classwork/classwork7.py
@@ -0,0 +1,13 @@
+first_number = (int(input("enter first number :")))
+second_number = (int(input("enter second number :")))
+
+print(first_number + second_number)
+print(first_number - second_number)
+print(first_number * second_number)
+print(first_number / second_number)
+print(first_number // second_number)
+print(first_number > second_number)
+print(first_number < second_number)
+print(first_number == second_number)
+
+print(int(9.0))
---
+++ Day07-Homework/lesson7.py
@@ -0,0 +1,44 @@
+#declaring number variables
+first_number = int(input("enter first number :"))
+second_number = int(input("enter second number :"))
+
+#print number variables
+print(first_number + second_number)
+print(first_number - second_number)
+print(first_number * second_number)
+print(first_number / second_number)
+print(first_number // second_number)
+print(first_number % second_number)
+print(first_number > second_number)
+print(first_number < second_number)
+print(first_number == second_number)
+
+#print int,str,float (test)
+print(int(9.0))
+print(int(5.0) + 6)
+
+print(float("7.6"))
+print(float(5))
+
+print(str(6))
+print(str(6.4))
+
+
+#how to get information about variable type
+
+#str
+print(type("5"))
+
+#int
+print(type(5))
+
+#float
+print(type(5.0))
+
+#boolean
+print(type(True))
+
+#list
+print(type([5]))
+
+
